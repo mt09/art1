7e0338a3c19c9608c0c2366fd2096add3325d1ee
wen <sunzibinfa@gmail.com>
2016-06-17 17:36:08 +0800

    admin/users/index, 權限轉換

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :admin do
     resources :products
+    resources :users do
+      member do
+        post :to_normal
+        post :to_admin
+      end
+    end
   end
 
   resources :products
