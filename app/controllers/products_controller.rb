b1bbc1cdb7db26c25f5e360cf07f34343aa8d58d
wen <sunzibinfa@gmail.com>
2016-06-17 18:03:06 +0800

    商品加入購物車, 已加入的商品無法重複加入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,17 @@ class ProductsController < ApplicationController
 	def show
 		@product = Product.find(params[:id])
 	end
+
+	def add_to_cart
+		@product = Product.find(params[:id])
+
+		if !current_cart.items.include?(@product)
+			current_cart.add_product_to_cart(@product)
+			flash[:notice] = "成功將商品 #{@product.title} 加入購物車"
+		else
+			flash[:warning] = "此商品已在購物車中"
+		end
+		
+		redirect_to product_path(@product)
+	end
 end
