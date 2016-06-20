51843fb875e2d6a517418527126967fe29307ef3
wen <sunzibinfa@gmail.com>
2016-06-20 12:57:24 +0800

    整額金流到rails project

--- config/initializers/pay2go.rb
+++ config/initializers/pay2go.rb
@@ -1,15 +1,17 @@
 Pay2go.setup do |pay2go|
-	if Rails.env.development?
-		pay2go.merchant_id = "12143236"
+  if Rails.env.development?
+    pay2go.merchant_id = "12143236"  # 放測試站的 key
 
-		pay2go.hash_key = "ozYlHVYvYP4ofE8Oj5BqP7j1wBsd9oKx"
-		pay2go.hash_iv = "YITGERbDGg1VV9Jf"
-		pay2go.service_url = "https://capi.pay2go.com/MPG/mpg_gateway"
-	else
-		pay2go.merchant_id = "12143236"
 
-		pay2go.hash_key = ""
-		pay2go.hash_iv = ""
-		pay2go.service_url = "https://api.pay2go.com/MPG/mpg_gateway"
-	end
+    pay2go.hash_key    = "ozYlHVYvYP4ofE8Oj5BqP7j1wBsd9oKx"
+    pay2go.hash_iv     = "YITGERbDGg1VV9Jf"
+    pay2go.service_url = "https://capi.pay2go.com/MPG/mpg_gateway"
+  else
+    pay2go.merchant_id = "1234567"  # 放正式站的 key
+
+
+    pay2go.hash_key    = "xxxxxxxx"
+    pay2go.hash_iv     = "xxxxxxxx"
+    pay2go.service_url = "https://api.pay2go.com/MPG/mpg_gateway"
+  end
 end
